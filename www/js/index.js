573436b0c03bad62d554167afe37dfe48213c96a
Luiz Alexandre <luiz.alexandre@live.com>
2016-11-10 21:47:34 -0200

    Basic Cordova

--- www/js/index.js
+++ www/js/index.js
@@ -1,18 +1,12 @@
 var app = {
-    // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
     },
 
-    // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
