cf4096d684881b1eef0f861e2c361873c0e755c3
Halyson Rezende <halyson97@gmail.com>
2016-11-14 12:04:13 -0200

    Refatoração usando VM e criação do card black

--- www/js/app.js
+++ www/js/app.js
@@ -2,30 +2,32 @@
 
 var spa = angular.module('appNoticias', ['ngRoute']);
 
-spa.controller('mainCtrl',['$scope', '$http','$timeout', function CardapioCtrl($scope, $http,$timeout) {
+spa.controller('mainCtrl',['$http','$timeout', function CardapioCtrl( $http,$timeout) {
+
+        let vm = this;
 
         $http({ 
-            url: "http://45.55.226.209/news/getLast", 
+            url: "http://45.55.226.209/news/getLasta", 
             dataType: 'json', 
             method:'GET',
-            headers: {'Content-Type': 'application/json'},
+            headers: {'Content-Type': 'application/json'}
         }).success(function (response) {
 
-        if (typeof(Storage) !== "undefined") {
-            localStorage.setItem("noticias", JSON.stringify(response.data));
-            $scope.noticias = JSON.parse(localStorage.getItem("noticias"));
-            console.log("pegando noticias da requisição");
+            if (typeof(Storage) !== "undefined") {
+                localStorage.setItem("noticias", JSON.stringify(response.data));
+                vm.noticias = JSON.parse(localStorage.getItem("noticias"));
+                console.log("pegando noticias da requisição");
 
-        } else {
-            $scope.noticias = response.data;
-        }
+            } else {
+                vm.noticias = response.data;
+            }
 
             console.log(response.data);
         }).error(function (response) {
 
             if (typeof(Storage) !== "undefined") {
                 if(JSON.parse(localStorage.getItem("noticias")) !== null){
-                    $scope.noticias = JSON.parse(localStorage.getItem("noticias"));
+                    vm.noticias = JSON.parse(localStorage.getItem("noticias"));
                     console.log("pegando noticias do localstorage");
                 }                 
             } else {
@@ -40,11 +42,11 @@ spa.controller('mainCtrl',['$scope', '$http','$timeout', function CardapioCtrl($
             splash.remove();
         }, 3000);
 
-        $scope.openInBrowser = function(url){
+        vm.openInBrowser = function(url){
             window.open(url);
         }
 
-        $scope.openSearch = function(){
+        vm.openSearch = function(){
             let toolbar = document.querySelector("#toolbar-search");
             toolbar.style.display = "block";
             let inp = document.querySelector("#inputSearch");
@@ -52,8 +54,8 @@ spa.controller('mainCtrl',['$scope', '$http','$timeout', function CardapioCtrl($
 
         }
 
-        $scope.closeSearch = function(){
-            $scope.searchUser = "";
+        vm.closeSearch = function(){
+            vm.searchUser = "";
             let toolbar = document.querySelector("#toolbar-search");
             toolbar.style.display = "none";
         }    
